74ec90b370e84c2914bf02b278828aff5a30cba1
lauraschoerken <lauraschorken@gmail.com>
2021-07-10 13:06:05 +0100

    making header unfinished

--- src/components/Header/Index.vue
+++ src/components/Header/Index.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="Header">
-    <header>hola xd</header>
+    <header>
+      <div>
+        <ul>
+          <li><a href="#"> Icon</a></li>
+          <li><a href="#/yorrecipes"> Tus Recetas</a></li>
+          <li><a href="#/reicpessearch"> Buscar Recetas</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li><a href="#/contact"> Contact</a></li>
+          <li><a href="#/perfil"> Perfil</a></li>
+        </ul>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
-export default ({
-  name: 'Header',
-  setup() {
-  },
-  components:{
-  }
-})
+export default {
+  name: "Header",
+  setup() {},
+  components: {},
+};
 </script>
+
+<style lang="scss" scoped>
+
+.header{
+  display: flex;
+  text-decoration: none;
+}
+
+
+</style>
