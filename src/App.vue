b15022050a762a8a62c12d6f7eba241a526afcde
David Hormiga Afonso <davidhormigafonso@gmail.com>
2021-07-10 12:44:00 +0100

    temporal links added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/error">Error</router-link> |
+      <router-link to="/contact">Contact</router-link> |
+      <router-link to="/main">Main</router-link> |
+      <router-link to="/profile">Profile</router-link> |
+      <router-link to="/recipesearch">Recipe Search</router-link> |
+      <router-link to="/yourrecipes">Your Recipes</router-link>
     </div>
     <router-view/>
   </div>
